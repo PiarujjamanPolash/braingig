de48c2a381bab2306ac089e4a7a0fecbddd9605d
Piarujjaman Polash <polash.sahel@gmail.com>
2025-09-04 16:42:07 +0600

    5

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -31,7 +31,7 @@ const Hero: React.FC = () => {
                     width={231}
                     height={100}
                   />{" "}
-                  Makes <span>Your Dream </span> Work
+                  Makes <span>Your Dream </span><br> Work</br>
                 </h2>
               </div>
             </div>
