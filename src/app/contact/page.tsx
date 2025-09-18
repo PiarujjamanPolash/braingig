aaad75691122afb96d4d5de7c7aea5a00f493b60
nusratnova5 <nusrat.nova16@gmail.com>
2025-09-19 01:01:39 +0600

    contact page dynamic import removed for map issue

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,12 +1,10 @@
-"use client"
-import dynamic from "next/dynamic";
+"use client";
 
-const BranchArea = dynamic(() => import("@/components/contact/BranchArea"));
-const MainArea = dynamic(() => import("@/components/contact/MainArea"));
-const MapArea = dynamic(() => import("@/components/contact/MapArea"), { ssr: false });
-const Breadcrumb = dynamic(() => import("@/components/shared/Breadcrumb"));
-const ClientWrapper = dynamic(() => import("@/components/wrappers/ClientWrapper"));
-
+import BranchArea from "@/components/contact/BranchArea";
+import MainArea from "@/components/contact/MainArea";
+import MapArea from "@/components/contact/MapArea";
+import Breadcrumb from "@/components/shared/Breadcrumb";
+import ClientWrapper from "@/components/wrappers/ClientWrapper";
 
 const ContactPage = () => {
     return (
