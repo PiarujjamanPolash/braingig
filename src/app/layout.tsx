222a0ff4706fa6ac17b0aa5bc581f2730c491f8d
nusratnova5 <nusrat.nova16@gmail.com>
2025-11-29 18:59:14 +0600

    new script tag added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import Header from "@/components/shared/Header";
 import { Toaster } from "react-hot-toast";
 import Script from "next/script";
 
-const bebasNeue  = Bebas_Neue({
+const bebasNeue = Bebas_Neue({
   weight: "400",
   subsets: ["latin"],
   variable: "--font-bebas",
@@ -34,6 +34,19 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-WXH28BXG');
+            `,
+          }}
+        />
+        <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-M0E65B3S7Y"
           strategy="afterInteractive"
         />
@@ -48,11 +61,20 @@ export default function RootLayout({
         </Script>
       </head>
       <body
-        className={`${bebasNeue .variable} ${plusJakarta.variable} antialiased`}
+        className={`${bebasNeue.variable} ${plusJakarta.variable} antialiased`}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WXH28BXG"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         <Header />
         {children}
-        <Toaster 
+        <Toaster
           position="top-right"
           toastOptions={{
             duration: 4000,
